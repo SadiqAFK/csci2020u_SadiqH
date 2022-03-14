23b87dadb2d4d105af10806993aa08b68b6155a8
SadiqAFK <59121975+SadiqAFK@users.noreply.github.com>
2022-03-14 10:10:37 -0400

    forgot to commit lab4 and 5

---
+++ lab5/src/main/java/com/example/lab5/HelloApplication.java
@@ -0,0 +1,69 @@
+package com.example.lab5;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        Scene scene = new Scene(grid, 320, 240);
+        stage.setTitle("Student Records");
+
+        TableView table = new TableView();
+        TableColumn id = new TableColumn("SID");
+        id.setCellValueFactory(
+                new PropertyValueFactory<StudentRecord, String>("studentID"));
+
+        TableColumn assignments = new TableColumn("Assignments");
+        assignments.setCellValueFactory(
+                new PropertyValueFactory<StudentRecord, String>("assignments"));
+
+        TableColumn midterm = new TableColumn("Midterm");
+        midterm.setCellValueFactory(
+                new PropertyValueFactory<StudentRecord, String>("midterm"));
+
+        TableColumn exam = new TableColumn("Final Exam");
+        exam.setCellValueFactory(
+                new PropertyValueFactory<StudentRecord, String>("finalExam"));
+
+        TableColumn mark = new TableColumn("Final Mark");
+        mark.setCellValueFactory(
+                new PropertyValueFactory<StudentRecord, String>("finalMark"));
+
+        TableColumn letterGrade = new TableColumn("Letter Grade");
+        letterGrade.setCellValueFactory(
+                new PropertyValueFactory<StudentRecord, String>("letterGrade"));
+
+        DataSource data = new DataSource();
+        table.setItems(data.getAllMarks());
+
+        table.getColumns().addAll(id, assignments, midterm, exam, mark, letterGrade);
+
+
+        grid.add(table, 0,0);
+
+
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ lab5/src/main/java/com/example/lab5/StudentRecord.java
@@ -0,0 +1,61 @@
+package com.example.lab5;
+
+public class StudentRecord {
+
+    String studentID;
+    float midterm,assignments,finalExam,finalMark;
+    char letterGrade;
+
+    public StudentRecord(String studentID, float assignments, float midterm, float finalExam) {
+        this.studentID = studentID;
+        this.midterm = midterm;
+        this.assignments = assignments;
+        this.finalExam = finalExam;
+        getFinals();
+    }
+
+    private void getFinals() {
+
+        this.finalMark = (this.assignments * 0.2f) + (this.midterm * 0.3f) + (this.finalExam * 0.5f);
+
+        if(this.finalMark <= 49){
+            this.letterGrade = 'F';
+        }
+        else if(this.finalMark <= 59){
+            letterGrade = 'D';
+        }
+        else if(finalMark <= 69){
+            this.letterGrade = 'C';
+        }
+        else if(finalMark <= 79){
+            this.letterGrade = 'B';
+        }
+        else{
+            this.letterGrade = 'A';
+        }
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public float getMidterm() {
+        return midterm;
+    }
+
+    public float getAssignments() {
+        return assignments;
+    }
+
+    public float getFinalExam() {
+        return finalExam;
+    }
+
+    public float getFinalMark() {
+        return finalMark;
+    }
+
+    public char getLetterGrade() {
+        return letterGrade;
+    }
+}
